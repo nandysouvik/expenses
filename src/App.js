49aef025b13d01c3f8cedebd54a23708cce5bfa2
Souvik Nandy <souvik.nandy@bdb.ai>
2025-08-14 00:59:55 +0530

    ui changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
+function App() {
+  const expenses = [
+    { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2024, 7, 14) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2024, 2, 12) },
+    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2024, 2, 28) },
+    { id: 'e4', title: 'New Desk', amount: 450, date: new Date(2024, 5, 12) }
+  ];
+
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', color: '#fff' }}>Expense Tracker</h1>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
